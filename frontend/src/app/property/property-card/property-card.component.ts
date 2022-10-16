f247f963a2d56a730beaa8574a2bdc97fab7334d
Thomas Crowe <wcrowe@g.clemson.edu>
2022-10-16 15:19:44 -0400

    Setup property card and default image.

--- frontend/src/app/property/property-card/property-card.component.ts
+++ frontend/src/app/property/property-card/property-card.component.ts
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-card.component.css']
 })
 export class PropertyCardComponent implements OnInit {
+  Property: any = {
+    "Id": 1,
+    "Name": "Sample House",
+    "Image": "assets/default-house.jpg",
+    "Type": "House",
+    "Bedrooms": 3,
+    "Bathrooms": 2,
+    "Price": 120000
+  }
 
   constructor() { }
 
